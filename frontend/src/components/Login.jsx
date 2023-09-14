8d57d16fb5434e82db31877b3e5085fae7d6de3e
CalBlanco <72684566+Jimdangle@users.noreply.github.com>
2023-09-14 10:32:45 -0700

    made added py-1 and px-6 classes to the input fields of login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -37,10 +37,10 @@ export default function Login(){
                 <div id="login-card" className="w-[32rem] h-[24rem] text-center bg-blue-300 rounded-b-lg rounded-t-md shadow-lg">
                 <h2 className="text-3xl font-bold text-white underline underline-offset-9 pt-5">BoatLog</h2>
                 <div className='relative pt-6 top-1/5'>
-                    <input name="email" onChange={handleChange}  className="focus:form-active-input form-nonactive-input duration-150"  type="email" placeholder="Email"></input>
+                    <input name="email" onChange={handleChange}  className="px-6 py-1 focus:form-active-input form-nonactive-input duration-150"  type="email" placeholder="Email"></input>
                     <br></br>
                     <br></br>
-                    <input name="pass" onChange={handleChange} required type="password" placeholder="Password" className="focus:form-active-input form-nonactive-input duration-150"></input>
+                    <input name="pass" onChange={handleChange} required type="password" placeholder="Password" className="px-6 py-1 focus:form-active-input form-nonactive-input duration-150"></input>
                     <br></br>
                     <br></br>
                     <button className='rounded-3xl bg-slate-100 p-5 font-semibold' onClick={handleLogin}>Login</button>
