acfc324cb38892327f3941685281d5d6b23540b7
CalBlanco <72684566+Jimdangle@users.noreply.github.com>
2023-09-14 16:16:10 -0700

    Created a view for Login and Signup

---
+++ frontend/src/views/LoginSignup.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import Login from '../components/Login/Login'
+import Signup from '../components/Signup/Signup'
+export default function LoginSignup() {
+  const [isUser, setIsUser] = useState(false);
+
+  const toggleUser = () => {
+    var isU = isUser;
+    setIsUser(!isU);
+  }
+  
+
+  return (
+    <>
+      
+      <button disabled={false} onClick={()=>{toggleUser()}} className='my-2 absolute top-3/4 left-1/2 -translate-x-1/2 translate-y-10 rounded-3xl bg-slate-100 p-5 font-semibold hover:bg-green-400'>Have An Account?</button>
+      
+      {isUser ? <Login></Login> : <Signup></Signup>}
+      
+    </>
+  )
+}
