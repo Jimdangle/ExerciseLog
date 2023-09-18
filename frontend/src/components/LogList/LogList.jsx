4eb1b48ce4fecdda10e840332e34be142580346c
CalBlanco <72684566+Jimdangle@users.noreply.github.com>
2023-09-18 13:44:49 -0700

    LogList is getting real results from workout/ls will need to change to users workouts

--- frontend/src/components/LogList/LogList.jsx
+++ frontend/src/components/LogList/LogList.jsx
@@ -1,20 +1,60 @@
 
 // Make more pretty
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
-export default function LogList({list}){
+export default function LogList({list, token}){
+
+    
+
+
     const [displayList, setDisplayList] = useState(list);
 
+    async function GetList(){
+        console.log(token)
+        try{
+            const response = await fetch("http://127.0.0.1:3001/workout/ls",{
+                method: "GET",
+                headers: {
+                    'Origin': 'http://127.0.0.1:3000',
+                    'Content-Type': 'application/json',
+                    'authorization': token
+                },
+                mode:'cors'
+            })
+
+            const bod = await response.json();
+            if(response.ok){
+                if(bod.all){
+                    setDisplayList(bod.all)
+                }
+            }
+            else{
+                setDisplayList([])
+            }
+        }
+        catch(e){
+            console.log(e.message);
+        }
+    }
+
+    useEffect(()=>{
+        GetList();
+    },[])
 
     function searchLogs(search_term){
-        const searched = list.filter( (item) => item.name.indexOf(search_term) != -1)
-        console.log(searched);
-        setDisplayList(searched);
+        if(search_term!=""){
+            const searched = displayList.filter( (item) => item.name.indexOf(search_term) != -1)
+            console.log(searched);
+            setDisplayList(searched);
+        }
+        else{
+            GetList();
+        }
     }
 
     //Idk if this is even actually needed
     function resetLogs(){
-        setDisplayList(list);
+        GetList();
     }
 
 
@@ -25,11 +65,12 @@ export default function LogList({list}){
                 <input type="text" className='ml-6 my-2 focus:form-active-input form-nonactive-input duration-150' onChange={(value)=>{searchLogs(value.target.value)}}></input>
             </div>
             <ul>
-                {displayList.map((item) => {
+                {displayList.map((item,index) => {
                     return (
-                        <button key={item.key} className="my-6 py-3 px-2 w-full h-24 rounded-md shadow-md bg-blue-200">
-                            <h1 className="font-bold text-lg">{item.name} <span>{item.created_at}</span></h1>
-                            <p>{item.key}</p>
+                        <button key={index} className="my-6 py-3 px-2 w-full h-32 rounded-md shadow-md bg-blue-200">
+                            <h1 className="font-bold text-lg">{item.name}</h1>
+                            <p>Created:{item.createdAt}</p>
+                            <p>Edited:{item.updatedAt}</p>
                         </button>
                     )
                 })}
--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -10,9 +10,11 @@ export default function Home({signout}){
         signout();
     }
 
+    const token = localStorage.getItem(lsPrefix+"actk");
+
     return(<>
         <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 text-4xl font-bold text-blue-900">You are logged in!</h1>
         <button className="rounded-3xl bg-slate-100 p-5 font-semibold hover:bg-green-400" onClick={logout}>Sign out</button>
-        <LogList list={[{name:"t1",created_at:"1",key:1},{name:"t2",created_at:"2",key:2},{name:"t3",created_at:"3",key:3}]}></LogList>
+        <LogList list={[{name:"t1",created_at:"1",key:1},{name:"t2",created_at:"2",key:2},{name:"t3",created_at:"3",key:3}]} token={token}></LogList>
     </>)
 }
