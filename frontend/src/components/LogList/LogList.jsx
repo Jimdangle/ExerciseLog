da5c44b9d8a836230d3298c2982f1c2f96a45aee
CalBlanco <72684566+Jimdangle@users.noreply.github.com>
2023-09-18 14:53:29 -0700

    Added in a way to view a particular workout

--- frontend/src/components/LogList/LogList.jsx
+++ frontend/src/components/LogList/LogList.jsx
@@ -2,48 +2,22 @@
 // Make more pretty
 import {useState, useEffect} from 'react';
 
-export default function LogList({list, token}){
+export default function LogList({GetList, SelectPage}){
 
-    
-
-
-    const [displayList, setDisplayList] = useState(list);
-
-    async function GetList(){
-        console.log(token)
-        try{
-            const response = await fetch("http://127.0.0.1:3001/workout/ls",{
-                method: "GET",
-                headers: {
-                    'Origin': 'http://127.0.0.1:3000',
-                    'Content-Type': 'application/json',
-                    'authorization': token
-                },
-                mode:'cors'
-            })
-
-            const bod = await response.json();
-            if(response.ok){
-                if(bod.all){
-                    setDisplayList(bod.all)
-                }
-            }
-            else{
-                setDisplayList([])
-            }
-        }
-        catch(e){
-            console.log(e.message);
-        }
-    }
+    const [displayList, setDisplayList] = useState([]);
 
     useEffect(()=>{
-        GetList();
+        GetList(setDisplayList);
     },[])
+   
+    
+    
+    
 
     function searchLogs(search_term){
         if(search_term!=""){
             const searched = displayList.filter( (item) => item.name.indexOf(search_term) != -1)
+            console.log(search_term)
             console.log(searched);
             setDisplayList(searched);
         }
@@ -51,12 +25,6 @@ export default function LogList({list, token}){
             GetList();
         }
     }
-
-    //Idk if this is even actually needed
-    function resetLogs(){
-        GetList();
-    }
-
 
     return(<>
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3/4">
@@ -66,8 +34,9 @@ export default function LogList({list, token}){
             </div>
             <ul>
                 {displayList.map((item,index) => {
+                    
                     return (
-                        <button key={index} className="my-6 py-3 px-2 w-full h-32 rounded-md shadow-md bg-blue-200">
+                        <button key={index} className="my-6 py-3 px-2 w-full h-32 rounded-md shadow-md bg-blue-200" onClick={()=>{SelectPage(item)}}>
                             <h1 className="font-bold text-lg">{item.name}</h1>
                             <p>Created:{item.createdAt}</p>
                             <p>Edited:{item.updatedAt}</p>
--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,5 +1,6 @@
 import lsPrefix from "../config/cfUtil"
 import LogList from "../components/LogList/LogList";
+import LogView from "./LogView";
 //Home Show recent workouts and let user edit items in the list
 // Two Components I think
 // 1) Log List : displays a list of workout logs with edit buttons and delete buttons
@@ -15,6 +16,6 @@ export default function Home({signout}){
     return(<>
         <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 text-4xl font-bold text-blue-900">You are logged in!</h1>
         <button className="rounded-3xl bg-slate-100 p-5 font-semibold hover:bg-green-400" onClick={logout}>Sign out</button>
-        <LogList list={[{name:"t1",created_at:"1",key:1},{name:"t2",created_at:"2",key:2},{name:"t3",created_at:"3",key:3}]} token={token}></LogList>
+        <LogView token={token}></LogView>
     </>)
 }
