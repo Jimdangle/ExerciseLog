16793e997a077bb69b5f5d02c414f892f32b9907
CalBlanco <72684566+Jimdangle@users.noreply.github.com>
2023-11-07 12:56:46 -0800

    Added in Green, Yellow, and Red versions of each outline seemed like the easiest way to go

--- frontend/src/pages/Summary/ImageTest.jsx
+++ frontend/src/pages/Summary/ImageTest.jsx
@@ -1,9 +1,18 @@
 
+
 export default function ImageTest({}){
     return (
         <div>
             <img className="absolute" src="MuscleOutlines/MuscleDiagram.png" alt="Main Diagram"/>
-            <img className="absolute border-solid border-2 border-ored" src="MuscleOutlines/Lats.png" alt="Lat Outline"/>
+            <img className="absolute" src="MuscleOutlines/LatsR.png" alt="Main Diagram"/>
+            <img className="absolute" src="MuscleOutlines/TrapsY.png" alt="Main Diagram"/>
+            <img className="absolute" src="MuscleOutlines/CalvesR.png" alt="Main Diagram"/>
+            <img className="absolute" src="MuscleOutlines/UpperPectoralsR.png" alt="Main Diagram"/>
+            <img className="absolute" src="MuscleOutlines/MiddlePectoralsG.png" alt="Main Diagram"/>
+            <img className="absolute" src="MuscleOutlines/LowerPectoralsR.png" alt="Main Diagram"/>
+            <img className="absolute" src="MuscleOutlines/FrontDeltoidsG.png" alt="Main Diagram"/>
+            <img className="absolute" src="MuscleOutlines/MiddleDeltoidsR.png" alt="Main Diagram"/>
+            
         </div>
     )
 }
